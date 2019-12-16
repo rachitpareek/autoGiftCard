e9361c9a03113bc8bb4a1b1ec770c0bc87095a88
Rachit Pareek <rachitpareek@berkeley.edu>
2019-12-15 19:09:17 -0800

    deployed successfully, now making this ig api endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,45 @@
+// const functions = require('firebase-functions');
+
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// // exports.helloWorld = functions.https.onRequest((request, response) => {
+// //  response.send("Hello from Firebase!");
+// // });
+
+'use strict';
+
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require ('path');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const index = require('./routes/index');
+const add = require('./routes/add');
+const PORT = process.env.PORT || 4000; 
+
+server.use(bodyParser());
+server.use(cors());
+server.use('/', index);
+server.use('/addUser', add);
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+server.engine('html', require('ejs').renderFile);
+
+var mongoDBURL = 'mongodb+srv://admin:admin@default-nnxo9.gcp.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDBURL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+
+let db = mongoose.connection;
+
+db.once('open', function () {console.log("Connected to database successfully!")});
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+server.listen(PORT);
+
+exports.app = functions.https.onRequest(server);
